6e61715e3ef9ec70c9acb099cb5b4f6e8d04a366
titouan-joseph <titouanjoseph@gmail.com>
2021-11-22 11:06:21 +0100

    Starting bluetooth connection
    Co-authored-by: gaetan-falcand <gaetan-falcand@users.noreply.github.com>

---
+++ Connection/Bluetooth/Bluetooth.ino
@@ -0,0 +1,42 @@
+#include <SoftwareSerial.h>
+#define rxPin 11 // Broche 11 en tant que RX, à raccorder sur TX du HC-05
+#define txPin 10 // Broche 10 en tant que TX, à raccorder sur RX du HC-05
+SoftwareSerial mySerial(rxPin, txPin);
+void setup()
+{
+    // define pin modes for tx, rx pins:
+    pinMode(rxPin, INPUT);
+    pinMode(txPin, OUTPUT);
+    mySerial.begin(38400);
+    Serial.begin(38400); 
+
+    if(mySerial.write("AT") == "OK"){
+        mySerial.write("AT+NAME=Ledz Go");
+        mySerial.write("AT+ROLE=1");
+        mySerial.write("AT+RESET");
+        mySerial.write("AT+INIT");
+    }
+
+}
+
+void loop()
+{
+    int i = 0;
+    char someChar[32] = {0};
+    // when characters arrive over the serial port...
+    if(Serial.available()) {
+    do{
+        someChar[i++] = Serial.read();
+        delay(3);
+    } while (Serial.available() > 0);
+    mySerial.println(someChar);
+    Serial.println(someChar);
+    }
+    while(mySerial.available())
+        Serial.print((char)mySerial.read());
+}
+
+String getBluetoothAddr(){
+    mySerial.write("AT+ADDR");
+
+}
---
+++ Connection/Bluetooth/README.md
@@ -0,0 +1,8 @@
+Documentation
+--
+
+https://www.gotronic.fr/pj2-guide-de-mise-en-marche-du-module-bluetooth-hc-1546.pdf
+
+https://www.aranacorp.com/fr/votre-arduino-communique-avec-le-module-hc-05/
+
+https://wikilab.myhumankit.org/index.php?title=Projets:Communication_Bluetooth_HC_05
