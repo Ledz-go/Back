27bce87da433fe1019fdf03b32cbe1eac912c846
jzhao1998 <46528670+jzhao1998@users.noreply.github.com>
2022-01-09 21:01:57 +0100

    Update ledz_go_master.ino

--- ledz_go_master.ino
+++ ledz_go_master.ino
@@ -1,63 +1,160 @@
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+
+//bluetooth
+/*
+#include <SoftwareSerial.h>
+#define rxPin D2 // Broche D2 en tant que RX, à raccorder sur TX du HC-05
+#define txPin D3 // Broche D3 en tant que TX, à raccorder sur RX du HC-05
+#define keyPin D4 // Broche D4 en tant que key, à raccorder sur Key du HC-05
+SoftwareSerial BTSerial(rxPin, txPin);
+*/
+
+
 unsigned long horloge;
 DynamicJsonDocument functionActuel(1024);
 DynamicJsonDocument functionRun(1024);
-//initialization as mode terminate and waiting for the message Discovery to start, 0 for run,1 for discovery, 2 for terminate
-int masterMode=2;
-//maintenant tout est en Serial et on va le remplacer après
 
+DynamicJsonDocument nodes(1024);
+
+//initialization as mode terminate and waiting for the message Discovery to start, 0 for run,1 for discovery, 2 for terminate
+int masterMode=1;
+//definition wifi
+String ssid = "ledzGO";
+String psk = "password";
+
+IPAddress local_IP(192,168,4,1);
+IPAddress gateway(192,168,4,1);
+IPAddress subnet(255,255,255,0);
+IPAddress Broadcast(192,168,4,255);
+unsigned int localPort = 8888;
+boolean hidden = false;
+int channel = 10;
+int max_connection = 8;
+WiFiUDP Udp;
+
+
+
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE + 1]; //buffer to hold incoming packet,
+char  ReplyBuffer[] = "acked\r\n";       // a string to send back
+char testData[]="{\"Version\": 1.3,\"Timestamp\": \"2012-04-23T18:25:43.511Z\",\"SessionId\": 486316,\"Data\": {\"Type\": \"Command\",\"NodeID\": 2354,\"Channels\": [ 125, 0, 62, 214 ]}}";
+//maintenant tout est en Serial et on va le remplacer après
+void sendCommand(DynamicJsonDocument json){
+  if(json["Data"]["NodeID"]==WiFi.macAddress().c_str()){
+    //DMX show
+    Serial.println("DMX Command");
+    int RGB[]={json["Data"]["Channels"][0],json["Data"]["Channels"][1],json["Data"]["Channels"][2],json["Data"]["Channels"][3]};
+    Serial.println(RGB[0]);
+    Serial.println(RGB[1]);
+    Serial.println(RGB[2]);
+    Serial.println(RGB[3]);
+  }else{
+    //Udp.beginPacket(nodes[receivedData["Data"]["NodeID"]], localPort);
+    Udp.beginPacket(Broadcast, localPort);
+    char testData[UDP_TX_PACKET_MAX_SIZE + 1];
+    serializeJson(json,testData);
+    Udp.println(testData);
+    Udp.endPacket();
+  }
+}
 void runFunctionActuel(){
   Serial.println("run function");
-  if(functionActuel[0]==null){
+  if(functionActuel["Function"][0]==NULL){
     horloge=millis();
-    copyArray(functionActuel,functionRun);
+    functionRun["Function"]=functionActuel["Function"];
   }
-  if(millis()-horloge>=functionRun[0]["time"]){
-    //sendRGB(funcionRun[0]);
+  if(millis()-horloge>=functionRun["Function"][0]["time"]){
+    DynamicJsonDocument command(1024);
+    DynamicJsonDocument commandJson(1024);
+    command["NodeID"]=functionRun["Function"][0]["NodeID"];
+    command["Channels"]=functionRun["Function"][0]["Channels"];
+    commandJson["Data"]=command;
+    sendCommand(commandJson);
     functionActuel.remove(0);
   }
 }
+
 void treatJson(DynamicJsonDocument json){
   if(json["Data"]["Type"]=="Function"){
-      Serial.println("Run Function");
-      //restart horloge
-      horloge=millis();
-    }else if(json["Data"]["Type"]=="Command"){
-      Serial.println("DMX Command");
-      int RGB[]={json["Data"]["Channels"][0],json["Data"]["Channels"][1],json["Data"]["Channels"][2],json["Data"]["Channels"][3]};
-      Serial.println(RGB[0]);
-      Serial.println(RGB[1]);
-      Serial.println(RGB[2]);
-      Serial.println(RGB[3]);
-    }else if(json["Data"]["Type"]=="Terminate"){
-      masterMode=2;
-      Serial.println("Terminate Mode");
-    }else if(json["Data"]["Type"]=="Discovery"){
-      masterMode=1;
-      Serial.println("Discovery Mode");
-    }else{
-      Serial.println("Wrong Type of JSON");
-    }
+    Serial.println("Run Function");
+    //restart horloge
+    horloge=millis();
+    functionActuel["Function"]=json["Data"]["Sequence"];
+    functionRun["Function"]=json["Data"]["Sequence"];
+    runFunctionActuel();
+  }else if(json["Data"]["Type"]=="Command"){
+    sendCommand(json);
+  }else if(json["Data"]["Type"]=="Terminate"){
+    masterMode=2;
+    Serial.println("Terminate Mode");
+  }else{
+    Serial.println("Wrong Type of JSON");
+  }
 }
 
 void setup()
 {
   Serial.begin(115200);  
+  WiFi.softAPConfig(local_IP, gateway, subnet);
+  Serial.println(WiFi.softAP(ssid, psk, channel, hidden, max_connection) ? "AP ready" : "AP failed");
+  Udp.begin(localPort);
+  Serial.println("Begin");
+  nodes[WiFi.macAddress().c_str()]=WiFi.localIP().toString().c_str();
+
+  //bluetooth
+  /*
+    // define pin modes for tx, rx pins:
+    pinMode(rxPin, INPUT);
+    pinMode(txPin, OUTPUT);
+
+    BTSerial.begin(9600);
+    Serial.begin(9600); 
+
+    // setup key pin
+    pinMode(keyPin, OUTPUT);
+    digitalWrite(keyPin, LOW); // key pin
+  */
+  horloge=millis();
 }
 
 void loop() {
-  //maintenant j'ai mis Serial mais ici ça devais remplacé par le reception BlueTooth
-  if(Serial.available()){
-    String read;
-    DynamicJsonDocument json(1024);
-    read=Serial.readString();
-    deserializeJson(json,read);
-    treatJson(json);
-    
-  }else{
-    runFunctionActuel();
+  if(masterMode==2){
     delay(1000);
-  }
+  }else if(masterMode==0){
+
+    //bluetooth
+    /*
+     if (BTSerial.available())
+      Serial.write(BTSerial.read());
   
+    // Keep reading from Arduino Serial Monitor 
+    //  and send to HC-05
+    if (Serial.available())
+      treatJson(receivedData);
+    */
+    DynamicJsonDocument receivedData(1024);
+    deserializeJson(receivedData,testData);
+    //treatJson(receivedData);
+    delay(1000);
+  }else{
+    int packetSize = Udp.parsePacket();
+    if (packetSize) {
+      Serial.println("DiscoveryMode");
+      int n = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      packetBuffer[n] = 0;
+      Serial.println(packetBuffer);
+      char IP[16];
+      char Mac[16];
+      sscanf(packetBuffer,"%s %s",IP,Mac);
+      Serial.println(IP);
+      Serial.println(Mac);
+      nodes[Mac]=IP;
+    }
+    if(millis()-horloge>60000){
+      masterMode=0;
+    }
+  }
 }
